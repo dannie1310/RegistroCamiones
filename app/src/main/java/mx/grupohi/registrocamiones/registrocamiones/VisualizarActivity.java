f63c518a689e02b9d566df5e4ba29713e8f84eb4
DBenitezC <DBenitezC>
2017-04-07 13:51:11 -0500

    quitar menu superior

--- app/src/main/java/mx/grupohi/registrocamiones/registrocamiones/VisualizarActivity.java
+++ app/src/main/java/mx/grupohi/registrocamiones/registrocamiones/VisualizarActivity.java
@@ -480,21 +480,6 @@ public class VisualizarActivity extends AppCompatActivity implements NavigationV
         }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
